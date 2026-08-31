e20542513203d62d0aa22e80dad0e43ebbbb174c
cgozde <cemreuyar0@gmail.com>
2026-08-31 14:29:25 +0300

    Add port 8000 to Dockerfile

--- src/api/main.py
+++ src/api/main.py
@@ -6,6 +6,13 @@ from generate import generate_answer
 
 app = FastAPI()
 
+from fastapi.responses import HTMLResponse
+
+@app.get("/", response_class=HTMLResponse)
+def home():
+    with open("src/api/static/index.html", "r", encoding="utf-8") as f:
+        return f.read()
+
 @app.get("/ask")
 def ask(soru: str):
     cevap = generate_answer(soru)
